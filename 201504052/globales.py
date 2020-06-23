9b85a8672688c6b6689cf5f89874e4b677e1fd12
usac201504052 <2947056270101@ingenieria.usac.edu.gt>
2020-06-22 18:29:43 -0600

    Ajustes

--- 201504052/globales.py
+++ 201504052/globales.py
@@ -16,7 +16,7 @@ ARCHIVO_USUARIOS = 'usuarios'
 ARCHIVO_SALAS = 'salas'
 
 # JAPO Credenciales del broker
-#MQTT_HOST = "167.71.243.238"
+MQTT_HOST = "167.71.243.238"
 MQTT_HOST = "127.0.0.1"
 MQTT_PORT = 1883
 
