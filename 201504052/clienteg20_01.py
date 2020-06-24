b18a2b19b13e3aa25e998764bed8237f767fa438
usac201504052 <2947056270101@ingenieria.usac.edu.gt>
2020-06-24 10:39:38 -0600

    Carga ultima

--- 201504052/clienteg20_01.py
+++ 201504052/clienteg20_01.py
@@ -2,10 +2,14 @@ import paho.mqtt.client as mqtt
 import logging
 import time
 import os 
-from globales import *
+import sys
 import threading
 import socket
 import binascii
+import wave
+
+sys.path.insert(0, '../')
+from globales import *
 
 # JAPO Configuracion de logging
 logging.basicConfig(
@@ -15,6 +19,30 @@ logging.basicConfig(
 # JAPO Funcion a ejecutar al establecer conexion
 def on_connect(client, userdata, flags, rc): 
     logging.info("Conectado al broker")
+    # JAPO Suscripcion al topic 'usuario'
+    archivoUsr = open(ARCHIVO_USUARIOS, 'r')
+    usr = []
+    for linea in archivoUsr.readlines():
+        usr = linea.split("\n")
+    topicUsuarios = "usuarios/20/" + usr[0]
+    topicAudio = "audio/20/" + usr[0]
+    usuario.subscribir((topicUsuarios, 2))
+    # JAPO Suscripcion al topic 'audio'
+    usuario.subscribir((topicAudio, 2))
+    archivoUsr.close()
+    # JAPO Suscripcion al topic 'salas'
+    archivo = open(ARCHIVO_SALAS, 'r')
+    t = []
+    s = []
+    for linea in archivo.readlines():
+        t = linea.split("20")
+        for i in range(1, len(t)):
+            s = t[i].split("\n")
+            topic = 'salas/' + str(NUMERO_GRUPO) + '/' + s[0]
+            topicAudSalas = 'audio/' + str(NUMERO_GRUPO) + '/' + s[0]
+            usuario.subscribir((topic, qos))
+            usuario.subscribir((topicAudSalas, qos))
+    archivo.close()
 
 # JAPO Funcion a ejecutar si se publica algo en el broker
 def on_publish(client, userdata, mid): 
@@ -23,9 +51,22 @@ def on_publish(client, userdata, mid):
 
 # JAPO Funcion a ejecutar si se recibe algo del broker
 def on_message(client, userdata, msg):
-    #Se muestra en pantalla informacion que ha llegado
+    # JAPO Se muestra en pantalla informacion que ha llegado
     logging.info("Tiene un mensaje en el topic: " + str(msg.topic))
-    logging.info("El contenido del mensaje es: " + str(msg.payload.decode()))
+    temas = []
+    temas = str(msg.topic).split('/')
+    if temas[0] == 'audio':             # JAPO Si el topic es de audio
+        content = []
+        content.append(msg.payload)     # JAPO Agrega el contenido binario del audio a un arreglo para su traspaso
+        # JAPO Creacion de hilo
+        tReproducir = threading.Thread(target= usuario.reproducirAudio,
+                                        name='Reproduccion', 
+                                        args=(content),
+                                    daemon= False
+                                    )
+        tReproducir.start()      # JAPO Inicia el hilo
+    else:
+        logging.info("El contenido del mensaje es: " + str(msg.payload.decode()))
 
 # JAPO Iniciar la conexion con el brocker
 client = mqtt.Client(clean_session=True)         # JAPO Nueva instancia de cliente MQTT
@@ -34,10 +75,6 @@ client.on_publish = on_publish                   # JAPO Funcion "Handler" a ejec
 client.on_message = on_message                   # JAPO Funcion "Handler" a ejecutar al llegar un mensaje a un topic subscrito
 client.username_pw_set(MQTT_USER, MQTT_PASS)     # JAPO Credenciales requeridas por el broker
 client.connect(host= MQTT_HOST, port = MQTT_PORT) # JAPO Conectar al servidor remoto
-
-# JAPO SOCK_STREAM = TCP
-#sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#server_address = (IP_ADDR, IP_PORT)  # JAPO Direccion IP y puerto en el que el servidor esta escuchando
 
 class cliente(object):
     # JAPO Clase con las funcionalidades de un cliente
@@ -53,19 +90,29 @@ class cliente(object):
     # JAPO Subscribirse a los topics necesarios
     def subscribir(self, topics = []):
         self.client.subscribe(topics)
+    
+    # JAPO Funcion para grabar el audio
+    def grabar(self, topic, duracion):
+        os.system('arecord -d {} -f U8 -r 8000 201504052_sent.wav'.format(duracion))
+        with open('201504052_sent.wav', 'rb') as audio:
+                enviar = audio.read()   # JAPO Se lee el archivo de audio en forma binaria
+                usuario.publicar(topic, bytearray(enviar))   # JAPO Se publica el archivo de audio en el topic
+                audio.close()        # JAPO Se cierra el archivo de audio
+
+    # JAPO Funcion para reproducir el audio
+    def reproducirAudio(self, contenido = []):
+        print("Reproduciendo")
+        with open("201504052_receive.wav", "wb") as rAudio:   # JAPO Se abre el archivo de audio donde se desea guardar audio
+            rAudio.write(contenido)                     # JAPO Se escribe el audio recibido sobre el archivo
+            rAudio.close
+        os.system('aplay 201504052_receive.wav') # JAPO Reproduce el archivo de audio
 
     # JAPO Terminar la comunicacion con mqtt
     def desconectar(self):
         self.client.disconnect()
 
-# JAPO Topics disponibles
-topicUsuarios = "usuarios/20/201504052"
-topicSalas = 'salas/20/'
-
 qos = 2
 usuario = cliente(client)                 # JAPO Instancia de objeto cliente
-#cmd = controlComandosCliente(client)     # JAPO Instancia de objeto de control de comandos
-usuario.subscribir((topicUsuarios, 2))
 
 client.loop_start()
 opcion = 0
@@ -75,28 +122,37 @@ try:
         if opcion == '1':
             op = input(" 1 - Enviar a un usuario individual \n 2 - Enviar a una sala \n")
             if op == '1':
-                userDest = input("Ingrese nombre de usuario \n")
+                userDest = input("Ingrese nombre de usuario \n") # JAPO Usuario de destino
                 # JAPO Configuracion topic usuarios
                 topic = 'usuarios/' + str(NUMERO_GRUPO) + '/' + str(userDest)
             elif op == '2':
-                salaDest = input("Ingrese a que sala desea enviarlo \n")
+                salaDest = input("Ingrese a que sala desea enviarlo \n")   # JAPO Sala de destino
                 # JAPO Configuracion topic salas
                 topic = 'salas/' + str(NUMERO_GRUPO) + '/' + str(salaDest)
-                usuario.subscribir((topic, qos))
+                #usuario.subscribir((topic, qos))
             msj = input("Ingese mensaje \n") # JAPO Mensaje a enviar
-            # JAPO Enviando por parametros el topic al que se desa enviar y el mensaje
             usuario.publicar(topic, msj.encode())   # JAPO El cliente en cuestion va a publicar el mensaje
             
         elif opcion == '2':
             op = input(" 1 - Enviar a un usuario individual \n 2 - Enviar a una sala \n")
             if op == '1':
-                userDest = input("Ingrese nombre de usuario \n")
-                duracion = input("Ingese duracion del mensaje de voz \n")
-
+                userDest = input("Ingrese nombre de usuario \n")  # JAPO Usuario de destino
+                # JAPO Configuracion topic usuarios
+                topic = 'audio/' + str(NUMERO_GRUPO) + '/' + str(userDest)
+            elif op == '2':
+                salaDest = input("Ingrese a que sala desea enviarlo \n")
+                # JAPO Configuracion topic salas
+                topic = 'audio/' + str(NUMERO_GRUPO) + '/' + str(salaDest)
+            duracion = input("Ingese duracion del mensaje de voz \n")   # JAPO Duracion del audio a grabar
+            # JAPO Instancia del hilo para grabacion de audio
+            tGrabar = threading.Thread(target= usuario.grabar,
+                                         name='Grabacion', 
+                                         args=(topic, duracion),
+                                       daemon= False
+                                      )
+            tGrabar.start()     # JAPO Inicia grabacion de audio en un hilo distinto
 except KeyboardInterrupt:
     logging.warning("Desconectando del broker MQTT...")
-    if talive.is_alive():
-        talive._stop()
 finally:
-    usuario.desconectar()
+    usuario.desconectar()   # JAPO Se desconecta el usuario del broker
     logging.info("Se ha desconectado del broker. Saliendo...")
